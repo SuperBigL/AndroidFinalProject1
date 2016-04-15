e7561b4b12d644525aee90bdb88dc5db17d7ab1e
SuperBigL <master_biglee@live.ca>
2016-04-15 10:17:30 -0600

    Started work on the stopWatch and finally decided on a Project: A stopwatch that doubles as a baton so that multiple people can practice in a relay race. When you're done, it will track the meters run, calories burnt, and times ran.

--- app/src/main/java/leban/nait/ca/finalproject/GraphResults.java
+++ app/src/main/java/leban/nait/ca/finalproject/GraphResults.java
@@ -5,17 +5,31 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 /**
  * Created by Owner on 2016-04-14.
  */
 public class GraphResults extends Activity
 {
+    private BarGraphSeries<DataPoint> series;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.graphresults.xml);
-        GraphView wethaerGraph = (GraphView)findViewById(R.id.weathergraph);
+        setContentView(R.layout.graphresults);
+        GraphView weatherGraph = (GraphView)findViewById(R.id.weathergraph);
+        series = new BarGraphSeries<DataPoint>();
+        Viewport viewport = weatherGraph.getViewport();
+        viewport.setXAxisBoundsManual(true);
+        viewport.setMinX(0);
+        viewport.setMaxX(30);
+        viewport.setMinY(0);
+        viewport.setMaxY(1000);
+        viewport.setScrollable(true);
     }
 }
